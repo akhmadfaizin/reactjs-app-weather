4eb64d41df70fa86af7f74aa8d6df6ad67297c95
Akhmad Faizin <24859207+akhmadfaizin@users.noreply.github.com>
2022-12-19 14:23:41 +0700

    Setting API
    Setting API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import API from "../src/services/api";
 
-function App() {
+function App(props) {
+  useEffect(() => {
+    // getGeoLocation();
+    getWeatherData();
+    //console.log("TES TES");
+  }, [props]);
+
+  const getGeoLocation = () => {
+    API.getGeoLocation("Rembang")
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log("ERRORnya : ", err);
+      });
+  };
+
+  const getWeatherData = () => {
+    API.getWeatherData("-6.7056856", "111.3486234")
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log("ERRORnya : ", err);
+      });
+  };
+
   return (
     <div className="App">
       <div>EMPTY PROJECT</div>
---
+++ src/services/api.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const API_KEY = "39b6860e6353cc7a2585c4589dc44086";
+const BASE_URL = "https://api.openweathermap.org/";
+
+const API_KEY_STRING = "&appid=" + API_KEY;
+
+const Get = (PATH) => {
+  const promise = new Promise((resolve, reject) => {
+    axios.get(`${BASE_URL}${PATH}${API_KEY_STRING}`).then(
+      (result) => {
+        // console.log(result);
+        // console.log(result.data);
+        resolve(result.data);
+      },
+      (err) => {
+        reject(err);
+        console.log(`Error Occur : ${err}`);
+      }
+    );
+  });
+  return promise;
+};
+
+// Weather Path
+const getGeoLocation = (city) => Get(`geo/1.0/direct?q=${city}&limit=5`);
+const getWeatherData = (lat, lon) =>
+  Get(`data/2.5/weather?lat=${lat}&lon=${lon}`);
+
+const API = {
+  getGeoLocation,
+  getWeatherData,
+};
+
+export default API;
